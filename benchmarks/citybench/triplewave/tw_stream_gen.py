fa3e2a1209b2abc43d86385e2806683058262450
riccardotommasini <tomma156@gmail.com>
2017-03-23 09:02:18 +0100

    properties stream file generator for tw

---
+++ benchmarks/citybench/triplewave/tw_stream_gen.py
@@ -0,0 +1,51 @@
+
+from string import Template
+from sys import argv
+
+
+host = argv[1]
+base_port = int(argv[2])
+base_ws_port = int(argv[3])
+base_url = argv[4]
+
+mapping_file = open( "properties.tmpl" )
+mapping_tmpl = Template( mapping_file.read() )
+
+
+pollution=["AarhusPollutionData201399",
+           "AarhusPollutionData184892",
+           "AarhusPollutionData182955",
+           "AarhusPollutionData158505",
+           "AarhusPollutionData206502",
+           "AarhusPollutionData179093",
+           "AarhusPollutionData195843",
+           "AarhusPollutionData206237",
+           "AarhusPollutionData195204",
+           "AarhusPollutionData204300"]
+
+parking=["AarhusParkingDataSKOLEBAKKEN",
+         "AarhusParkingDataKALKVAERKSVEJ"]
+
+traffic=["AarhusTrafficData158505",
+         "AarhusTrafficData182955",
+         "AarhusTrafficData186979",
+         "AarhusTrafficData192707"]
+
+
+for coll in ["pollution", "parking", "traffic"]:
+    for s in eval(coll):
+        m = { "stream":s,"stream_type":coll,
+              "host":host, "port":base_port,
+              "ws_loc":"stream", "ws_port":base_ws_port, 
+              "base_url":base_url}
+        print m
+        res = mapping_tmpl.substitute(m)
+        text_file = open(s+".properties", "w")
+        text_file.write(res)
+        text_file.close()
+        base_port +=1
+        base_ws_port +=1
+
+
+#print ("Finished")doc
+##USAGE python <stream name> <stream-mapping-template> <output-file>
