6718508cd24209937fc9283fd868189c3d5261ad
riccardotommasini <tomma156@gmail.com>
2017-04-21 16:28:35 +0200

    added comments and the automatic delection of old engine images

--- consumer/start.sh
+++ consumer/start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+#Hostname coincide with the engine to run csparql/cquels 
+#If necessary modifies /etc/hosts
+
 export ENGINE=$(hostname)
 
-docker pull streamreasoning/$ENGINE
+#Pull latest version of the engine
+docker pull streamreasoning/$ENGINE 
+#Delete the old (untagged) one
+docker images -f "dangling=true" -q 
 
+#Programmatically build the docker-compose file to run the engine 
 ./build
 
+#Run the engine and cadvisor
 docker-compose up --force-recreate -d
 
+#Create dashboard on the collector vm speaking with the running grafana instance (Port 3000)
 ./create-dashboards.sh
