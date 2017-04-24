e1b384b62e8445ac40a21f80b6fc8b292038cb0b
Ubuntu <rspdev@collector.tlplwnh2v4zevp0qxd4vea35rc.ax.internal.cloudapp.net>
2017-04-24 19:40:14 +0000

    fixed bugs in automated collector run

--- collector/monitor/start.sh
+++ collector/monitor/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #Create InfluxDB Shared Volume
+echo "docker volume create -d azurefile --name $1 -o share=$1"
 docker volume create -d azurefile --name $1 -o share=$1
 
 #Create Query Results Shared Volume
+echo "docker volume create -d azurefile --name $2 -o share=$2"
 docker volume create -d azurefile --name $2 -o share=$2
 
+echo "./build $1 $2"
 ./build $1 $2
 
 docker-compose up -d
