3ec00746658fa01ff9f01f4a92d85fc4045b9a9b
riccardotommasini <tomma156@gmail.com>
2017-04-26 17:08:30 +0200

    refactoring part 2

---
+++ benchmarks/citybench/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export MACHINE=$(hostname)
+
+echo $MACHINE
+
+docker rmi streamreasoning/twcitybench
+docker build -t  streamreasoning/twcitybench ./triplewave/
+
+rm -rf ./*.yml
+
+./scripts/build $@
+docker-compose up -d
+
+cat docker-compose.yml
+echo $@
+	
+./scripts/create-dashboards.sh $@
