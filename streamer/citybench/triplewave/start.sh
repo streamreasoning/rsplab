b67461faf7b4709d897424a4778682887efd1622
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2017-06-10 09:58:51 +0200

    changes files to work locally

---
+++ streamer/citybench/triplewave/start.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#. triplewave.properties
+ 
+
+while [[ "$#" > 1 ]]; do case $1 in
+    --configuration) configuration="$2";;
+    *) break;;
+  esac; shift; shift
+done
+
+if [ -z "$configuration" ]; then
+	configuration="config/config.properties"
+fi
+
+echo "Looking for the configuration file ${configuration}"
+
+echo "Reading the configuration file"
+while IFS='=' read -r key value; do
+   case $key in
+       ''|\#*) continue ;;         # skip blank lines and lines starting with #
+   esac
+   # stuff with var1, etc.
+   eval "${key}='${value}'"
+done < "${configuration}"
+
+while [[ "$#" > 0 ]]; do case $1 in
+  --mode) export mode="$2";;
+	--sources) export sources="$2";;
+	--debug) debug="true";;
+	--log) log="true";;
+    *) break;;
+  esac; shift; shift
+done
+
+echo "Starting Up..."
+if [ "$mode" != "transform"]; then
+	if [ "$sources" != "rdfstream" || "$sources" != "stream" ]; then
+     
+     echo "Starting up Fuseki..."
+	   cd fuseki
+	   java -jar jena-fuseki-server-2.3.1.jar --update --mem /ds &
+	   cd ..
+    echo "the Fuseki pid is $!"
+	   sleep 10
+	fi
+fi
+
+echo "Starting up TripleWave ..."
+
+if [ -z "${log}" ]; then
+  if [ -z "${debug}" ]; then
+    node app.js --fuseki=$! --mode="$mode" --configuration="$configuration" --sources="$sources"
+  else
+    echo "Starting in debug mode"
+    node debug app.js --fuseki=$! --mode="$mode" --configuration="$configuration" --sources="$sources"
+  fi
+else
+  if [ -z "${debug}" ]; then
+    DEBUG node app.js --fuseki=$! --mode="$mode" --configuration="$configuration" --sources="$sources"
+  else
+    echo "Starting in debug mode"
+    DEBUG node debug app.js --fuseki=$! --mode="$mode" --configuration="$configuration" --sources="$sources"
+  fi
+fi
+
