db67e4902aedbb3c18c9e9c3900f8540acbd29b6
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2017-10-19 23:20:09 +0200

    removed lsbench

--- streamer/lsbench/generation/generator/src/sib/testdriver/QualificationDefaultValues.java
+++
@@ -1,6 +0,0 @@
-package sib.testdriver;
-
-public class QualificationDefaultValues {
-	public static final boolean resultsCountOnly=false;
-	public static final String qualificationLog = "qual.log";
-}
