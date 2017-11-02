fea7f1fd9556984966da518bea64a2dc09d1e51a
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2017-11-02 15:31:35 +0100

    [Collector] updated dockerfile to version 3 and fixed localhost in grafana uri

--- collector/create-dashboards.sh
+++ collector/create-dashboards.sh
@@ -17,8 +17,8 @@
 #   create-dashboards.sh
 #
 
-INFLUXDB_API_URL='http://localhost:8086/'
-INFLUXDB_API_REMOTE_URL='http://localhost:8086/'      # url for commands proxied through Grafana
+INFLUXDB_API_URL='http://influxsrv:8086/'
+INFLUXDB_API_REMOTE_URL='http://influxsrv:8086/'      # url for commands proxied through Grafana
 INFLUXDB_ROOT_LOGIN='root'
 INFLUXDB_ROOT_PASSWORD='root'
 
